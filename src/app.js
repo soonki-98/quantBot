a33e678f47a6a5317bf7f494757bdacabea0a845
soonki-98 <alstnsrl98@gmail.com>
2023-04-08 15:29:07 +0900

    거래 스케줄링

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
+import { scheduleJob } from "node-schedule";
 import collectDatas from "./collectDatas.js";
 import trade from "./trade.js";
+import express from "express";
 
-collectDatas().then((data) => {
-  trade(data);
+const app = express();
+
+app.listen(3000, async () => {
+  scheduleJob("0 0 12 30 * *", async () => {
+    const data = await collectDatas();
+    trade(data);
+  });
 });
